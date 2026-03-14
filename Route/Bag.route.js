f836974585900fd3ae95999f49f844392ec89a00
Muzamil Nabi <Muzamin213@gmail.com>
2026-03-14 14:49:12 +0530

    remove the authorization

--- Route/Bag.route.js
+++ Route/Bag.route.js
@@ -2,7 +2,7 @@ import express from "express"
 import { AddBag, bagPatch, DeleteBagItem, GetBag, GetBagCount, UpdateBagItem } from "../controller/Bag.controller.js"
 import { authorize } from "../MiddleWare/auth.middleware.js";
 const router = express.Router()
-router.post("/",authorize, AddBag);
+router.post("/", AddBag);
 router.get("/getbag/:userId",authorize,GetBag)
 router.get("/count", authorize,GetBagCount)
 router.put("/updateBag/:itemId",UpdateBagItem)

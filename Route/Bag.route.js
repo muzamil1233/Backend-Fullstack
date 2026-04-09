10f2f49d6ea30af9f345c8f9519f4690611a6134
Muzamil Nabi <Muzamin213@gmail.com>
2026-04-09 17:31:04 +0530

    added authorization

--- Route/Bag.route.js
+++ Route/Bag.route.js
@@ -1,12 +1,14 @@
 import express from "express"
 import { AddBag, bagPatch, DeleteBagItem, GetBag, GetBagCount, UpdateBagItem } from "../controller/Bag.controller.js"
 import { authorize } from "../MiddleWare/auth.middleware.js";
-const router = express.Router()
-router.post("/", AddBag);
-router.get("/getbag/:userId",GetBag)
-router.get("/count",GetBagCount)
-router.put("/updateBag/:itemId",UpdateBagItem)
-router.patch("/patchBag/:itemId",bagPatch)
 
-router.delete("/deleteBag/:itemId",DeleteBagItem)
-export default router; 
+const router = express.Router()
+
+router.post("/", authorize, AddBag);
+router.get("/getbag/:userId", authorize, GetBag)
+router.get("/count", authorize, GetBagCount)
+router.put("/updateBag/:itemId", authorize, UpdateBagItem)
+router.patch("/patchBag/:itemId", authorize, bagPatch)
+router.delete("/deleteBag/:itemId", authorize, DeleteBagItem)
+
+export default router;

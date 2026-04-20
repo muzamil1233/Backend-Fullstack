b423f8276f4584cf48dba9238a17c14e0cb293d0
Muzamil Nabi <Muzamin213@gmail.com>
2026-04-20 17:13:59 +0530

    added authrizatuion

--- MiddleWare/auth.middleware.js
+++ MiddleWare/auth.middleware.js
@@ -1,20 +1,20 @@
- import jwt from "jsonwebtoken";
- 
- const authorize= async (req,res,next)=>{
-    try {
-       const token = req.headers.authorization.split(" ")[1];
-       if(!token)return res.status(404).json({msg : "unauthorrized"});
-       const user = await jwt.verify(token,process.env.JWT_SECRET)
-       req.user=user;
-       next()
-    } catch (error) {
-        console.log(error);   
-     return res.status(404).json({msg : 'Unauthorized'})
-    }
-}
-export {
-    authorize
-}
+import jwt from "jsonwebtoken";
 
+const authorize = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) return res.status(401).json({ msg: "Unauthorized" });
+    
+    const token = authHeader.split(" ")[1];
+    if (!token) return res.status(401).json({ msg: "Unauthorized" });
+    
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ msg: "Unauthorized" });
+  }
+};
 
-
+export { authorize };
